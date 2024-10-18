e57ddfe10a45331d36314cff2b71f8b552b512a2
Guillem Poy <guplem@gmail.com>
2024-10-18 20:02:22 +0200

    Proper data format - introduction done

--- js/dataFiller.js
+++ js/dataFiller.js
@@ -4,36 +4,50 @@ import remarkBreaks from "https://esm.sh/remark-breaks@4";
 import remarkRehype from "https://esm.sh/remark-rehype@8";
 import rehypeStringify from "https://esm.sh/rehype-stringify@7";
 
-fillIntroduction();
+const markdownToHtmlCache = new Map();
 
-async function fillIntroduction() {
-  console.log("👋🏽 Filling about me section");
+fillData("introduction", "../data/info.json", "introduction");
+fillData("aboutMe", "../data/info.json", "aboutMe");
 
-  // Get the introduction element
-  const introductionElement = document.getElementById("introduction");
-
-  if (!introductionElement) {
-    alert("Could not find introduction element");
-    return;
+async function markdownToHtml(markdown) {
+  if (markdownToHtmlCache.has(markdown)) {
+    return markdownToHtmlCache.get(markdown);
   }
 
-  // Get the data/info.json file
-  const response = await fetch("../data/info.json");
-  if (!response.ok) {
-    throw new Error("Network response was not ok");
+  const processedHtml = await unified().use(remarkParse).use(remarkBreaks).use(remarkRehype).use(rehypeStringify).process(markdown);
+
+  markdownToHtmlCache.set(markdown, processedHtml);
+  return processedHtml;
+}
+
+async function fillData(elementId, dataUrl, dataKey) {
+  console.log(elementId + " START) Filling data");
+
+  const element = document.getElementById(elementId);
+  if (!element) {
+    throw new Error("Could not find element with id " + elementId);
   }
-  const data = await response.json();
 
-  // Get the "introduction" data from the data/info.json file
-  const introductionData = data.introduction;
+  try {
+    const response = await fetch("../data/info.json");
+    if (!response.ok) {
+      throw new Error("Failed to fetch data from " + dataUrl);
+    }
+    const data = await response.json();
 
-  // Turn the data from markdown to HTML
-  //   const introductionHtml = micromark(introductionData);
+    var rawData = data[dataKey];
+    if (!rawData) {
+      throw new Error("Could not find data with key " + dataKey + " in " + dataUrl);
+    }
+    // if the rawData is an array, join it into a string with "\n\n" as separator
+    if (Array.isArray(rawData)) {
+      rawData = rawData.join("\n\n");
+    }
+    const dataFormatted = await markdownToHtml(rawData);
+    element.innerHTML = String(dataFormatted);
 
-  const introductionHtml = await unified().use(remarkParse).use(remarkBreaks).use(remarkRehype).use(rehypeStringify).process(introductionData);
-
-  // Add a paragraph to the element
-  introductionElement.innerHTML = introductionHtml;
-
-  console.log("👋🏽 About me section filled");
+    console.log(elementId + " END) Filling data");
+  } catch (error) {
+    console.error("Error filling data in element with id " + elementId, error);
+  }
 }
